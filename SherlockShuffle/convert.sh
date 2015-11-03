033ce1ea8ca76d2c12eaf53504757620c5690165
Joel Kirchartz <jkirchartz@gmail.com>
2015-11-02 23:39:04 -0500

    write sherlock holmes nabber

---
+++ SherlockShuffle/convert.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#
+# convert.sh
+#
+# Copyleft (ↄ) 2015 jkirchartz <jkirchartz@gmail.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+
+for file in ./sources/*.txt; do
+  title=$(grep Title: $file | cut -d' ' -f2- | tr ' ' '_' | head)
+  stripgutenberg.pl < $file > ./corpora/$title.txt
+done
+
+cat ./corpora/*.txt > ./corpora/complete.corpus
