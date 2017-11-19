4ceeac6e42606b4cd64883f5472c08fe43dbe738
Joel Kirchartz <jkirchartz@gmail.com>
2017-11-18 23:14:40 -0500

    glitch image, print message over it

--- 2017/Artbook/index.js
+++ 2017/Artbook/index.js
@@ -47,6 +47,8 @@ fs.readdir(dir, function (err, files) {
 		});
 		let sentence = markov.makeChain();
 		console.log('Page Text:');
+		sentence = sentence.split('.');
+		sentence = sentence[Math.floor(sentence.length * Math.random())];
 		console.log(sentence.trim());
 
 		let randomWord = sentence.split(' ');
@@ -59,13 +61,27 @@ fs.readdir(dir, function (err, files) {
 			license: '7,9,10'
 		}).then(function (res) {
 			var photo = res.body.photos.photo[Math.floor(Math.random() * res.body.photos.photo.length)];
-			var url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_m.jpg';
+			var url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_b.jpg';
 			console.log(url);
 			gleech.read(url).then( function (image) {
 				console.log('Glitching Image...');
-
+				image.preset(Math.floor(Math.random() * 5));
+				gleech.loadFont(gleech.FONT_SANS_32_BLACK).then(function (font) {
+							image.print(font, 9, 9, sentence.trim(), image.bitmap.width - 20);
+							image.print(font, 9, 11, sentence.trim(), image.bitmap.width - 20);
+							image.print(font, 11, 9, sentence.trim(), image.bitmap.width - 20);
+							image.print(font, 11, 11, sentence.trim(), image.bitmap.width - 20);
+							gleech.loadFont(gleech.FONT_SANS_32_WHITE).then(function (font) {
+								image.print(font, 10, 10, sentence.trim(), image.bitmap.width - 20);
+								image.write(randomWord.trim() + '.jpg');
+							}).catch(function (err) {
+								console.error('gleech.print error:', err);
+							});
+				}).catch(function (err) {
+					console.error('gleech.print error:', err);
+				});
 			}).catch(function (err) {
-
+				console.error('gleech.read error:', err);
 			});
 		}).catch(function (err) {
 			console.error('flickr error:', err);
