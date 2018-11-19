999736485ddae2d01e58901d418ac718ee66dcb4
JKirchartz <jkirchartz@gmail.com>
2018-11-19 00:24:56 -0500

    update wink scripts, improvev upon sherlock shuffle slightly -- still not the best tagging experience

--- 2018/Winkie/index.js
+++ 2018/Winkie/index.js
@@ -26,12 +26,23 @@ const parseSentence = (str, index) => {
   str = contractions.expand(str);
   // tag sentence
   let taggedSentence = tagger.tagSentence(str);
-  // add sentence to grammar
+  // fix peculiarity of wink-pos-tagger tagging sentences as NNP
+  let scragglers = [];
+  taggedSentence.filter((obj, index) => {
+    if (obj.pos.slice(0,3) === "NPP" &&
+       (obj.value.indexOf(" ") > -1 || obj.value.indexOf("\"") > -1)) {
+      scragglers.push(tagger.tagSentence(obj.value));
+      return false;
+    }
+    return true;
+  });
+  taggedSentence = taggedSentence.concat(scragglers);
+  // munge sentences to create tracery
   taggedSentence = taggedSentence.filter(tagifySentence);
-  return tagsToSentence(taggedSentence);
+  return tidySentences(taggedSentence);
 };
 
-const tagsToSentence = (arr) => {
+const tidySentences = (arr) => {
   // reduce array of objects to one object and return it's value
   return arr.reduce((a, b) => {
     // apply proper spacing, being mindful of punctuation
@@ -60,22 +71,33 @@ const tagifySentence = (obj, i, arr) => {
     traceryOutput[key].push(word);
   }
   if (traceryOutput[key].indexOf(word) > -1 ) {
-    //capitalize first letter in a sentence
+    obj.value = '#' + key + '#';
     if((i === 0 && obj.pos !== "\"") ||
       (i >= 1 && arr[i-1].pos === "\"")) {
+      //capitalize first letter in a sentence
       obj.value = '#' + key + '.capitalize#';
+    } else if (obj.pos.slice(0,3) === "NNP") {
+      // capitalize all proper nouns
+      obj.value = '#' + key + '.capitalizeAll#';
+    }
+    if (obj.lemma && obj.normal.slice(-2) === 'ed' && obj.lemma.slice(-2) !== 'ed') {
       // preserve "-ed" words
-    } else if (obj.lemma && obj.normal.slice(-2) === 'ed' && obj.lemma.slice(-2) !== 'ed') {
       obj.value = '#' + key + '.ed#';
+    }
+    if (obj.lemma && obj.normal.slice(-3) === 'ing' && obj.lemma.slice(-3) !== 'ing') {
       // preserve "-ing" words
-    } else if (obj.lemma && obj.normal.slice(-3) === 'ing' && obj.lemma.slice(-3) !== 'ing') {
       obj.value = '#' + key + '.ing#';
-    } else if (was_a_or_an) {
+    }
+    if (was_a_or_an) {
+      // ensure correct "A" or "An" is used
       obj.value = '#' + key + '.a#';
-    } else {
-      obj.value = '#' + key + '#';
     }
   }
+  // don't save one-letter non-words
+  if (obj.value.length === 1 &&
+      (word !== "a" || word !=="o" || word !== "i")) {
+      return false;
+    }
   if (word === "a" || word === "an") {
     was_a_or_an = true;
     return false;
@@ -85,42 +107,84 @@ const tagifySentence = (obj, i, arr) => {
   return obj;
 };
 
-// read corpus & write grammar
-fs.readFile('corpus.txt', (err, data) => {
-  if (err) {
-    console.err("Can't read file", err);
-    process.exit(2);
-  }
-  // get sentences
-  let sentences = util.string.sentences(data.toString());
-  sentences = sentences.map(parseSentence);
-  traceryOutput['sentences'] = sentences;
+const parseCorpus = () => {
+  fs.readFile('corpora/complete.corpus', (err, data) => {
+    if (err) {
+      console.err("Can't read file", err);
+      process.exit(2);
+    }
+    let corpus = data.toString();
+    corpus = corpus.split("\n\n");
+    corpus.forEach((para, index) => {
+      if(para.match(/"/g).length % 2 == 1 && (para.slice(1) === "\"" && para.slice(-1) !== "\"")){
+        para = para + "\"";
+      }
+    });
+    // get sentences
+    let sentences = util.string.sentences(corpus.join("\n\n"));
+    sentences = sentences.map(parseSentence);
+    traceryOutput['sentences'] = sentences;
 
-  fs.writeFile('taggedSentences.json', JSON.stringify(traceryOutput, null, 2), (err) => {
-    if (err) {return console.err('error: ', err);}
-    console.log('JSON written');
+    fs.writeFile('corpora/corpus.json', JSON.stringify(traceryOutput, null, 2), (err) => {
+      if (err) {return console.err('error: ', err);}
+      console.log('JSON written');
+    });
+    writeBook(traceryOutput);
   });
+};
+
+const writeBook = (traceryOutput) => {
+  traceryOutput = !!traceryOutput ? traceryOutput : fs.readFileSync('corpora/corpus.json');
 
   let grammar = tracery.createGrammar(traceryOutput);
-  console.log(tracery.baseEngModifiers);
   grammar.addModifiers(tracery.baseEngModifiers);
-  grammar.addModifiers({ing: (str) => {
-    if ( str.slice(-2) === 'ie') {
-      str = str.slice(-2) + "ing";
-    } else if ( str.slice(-1) === 'e') {
-      str = str.slice(-1) + "ing";
-    } else {
-      str = str + "ing";
+  grammar.addModifiers({
+    ing: (str) => {
+      // -inger -- won't properly double-letters with emphasis on ending consonant-vowel-consonant (e.g. will output "runing" instead of "running")
+      if ( str.slice(-2) === 'ie') {
+        str = str.slice(-2) + "ing";
+      } else if ( str.slice(-1) === 'e') {
+        str = str.slice(-1) + "ing";
+      } else {
+        str = str + "ing";
+      }
+      return str;
     }
-  }});
+  });
   let bookText = "";
   let bookLength = 0;
   while(bookLength < 50000) {
     bookText += grammar.flatten('#sentences#') + "\n";
+    // add in random paragraph breaks
+    if (Math.round(Math.random() * 100) < 10 ) {
+      bookText += "\n\n";
+    }
     bookLength = bookText.split(" ").length;
   }
-  fs.writeFile('book.txt', bookText, (err) => {
+  // determine filename for book
+  let bookOutputLocation = 'output/book.txt';
+  while (fs.existsSync(bookOutputLocation)) {
+    if ( /\d+/.test(bookOutputLocation) ) {
+      let bookNum = bookOutputLocation.match(/\d+/)[0];
+      bookOutputLocation = bookOutputLocation.split(bookNum);
+      bookNum = parseInt(bookNum, 10) + 1;
+      bookOutputLocation = bookOutputLocation[0] + (bookNum) + ".txt";
+    } else {
+      bookOutputLocation = bookOutputLocation.split('.');
+      bookOutputLocation = bookOutputLocation[0] + "_1.txt";
+    }
+  }
+  // write book
+  fs.writeFile(bookOutputLocation, bookText, (err) => {
     if (err) {return console.err('error: ', err);}
     console.log('Book written');
   });
-});
+};
+
+// read corpus & write grammar
+if (fs.existsSync('corpora/corpus.json')) {
+  writeBook();
+} else {
+  parseCorpus();
+}
+
---
+++ 2018/Winkie/make.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#
+# make.sh
+#
+# Copyleft (ↄ) 2018 kirch <kirch@arp>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+#
+
+mkdir corpora
+mkdir sources
+mkdir output
+
+echo "downloading..."
+wget -nc -i baum.list -P ./sources
+
+echo "converting...."
+for file in ./sources/*.txt; do
+  title=$(grep Title: "$file" | head | cut -d' ' -f2- | sed -e 's|[^A-Za-z]||g')
+  ./stripgutenberg.pl < "$file" > "./corpora/$title.txt"
+done
+
+cat ./corpora/*.txt > ./corpora/complete.corpus
+
+sed -i '/HTML/d' ./corpora/complete.corpus
+sed -i 's/--/ -- /g' ./corpora/complete.corpus
+
+npm install
+
+echo "Generating Text from Tracery Grammar..."
+node index.js
