0c5f7bfc2f48c87ea7f1dab11b4fe886644dde25
JKirchartz <jkirchartz@gmail.com>
2018-11-21 13:26:16 -0500

    get vocabuclept ready for primetime

--- 2018/vocabularyclept/index.js
+++ 2018/vocabularyclept/index.js
@@ -5,13 +5,14 @@
  * Distributed under terms of the NPL (Necessary Public License) license.
  */
 
-let syllables = require('syllable');
-let fs = require('fs');
-let request = require('request');
+const fs = require('fs');
+const request = require('request');
+const syllables = require('syllable');
 
-// todo: use this to loop all poems in the corpora directory, producing a single collected works
-// it's possible to use the syllable-count corpus for all texts, to increase its vocabulary
-// create a title-case converter to use on the first line of each poem -- give credit to the original author?
+// IDEAS:
+// use the syllable-count corpus for all texts, to increase its vocabulary
+// give credit to the original author?
+// find anagram or spoonerism or something for authors names?
 
 let titles = [];
 let title_index = 0;
@@ -32,9 +33,9 @@ function shuffle(a) {
 }
 
 let parsePoem = function(poem) {
-  let output = "";
   let lines = []
   let words = {};
+  // turn poem into grammar
   poem.forEach(function (line) {
     line = line.split(' ');
     line.forEach(function(word, index) {
@@ -52,17 +53,20 @@ let parsePoem = function(poem) {
     }
   });
 
+  // turn grammar into poem
   lines = lines.map(function(line) {
     line = line.map(function(word, index) {
-      // replace each word with line with another word of the same length
-      return words[word][Math.floor(Math.random() * words[word].length)];
+      if ( words[word] ) {
+        // replace each word with line with another word of the same length
+        return words[word][Math.floor(Math.random() * words[word].length)];
+      }
     });
     return line.join(' ');
   });
   titles.push(lines[0]);
-  lines[0] = "## " + lines[0].split(' ').map(capitalize)  + "\n\n"
-  output += lines.join('  \n') + "\n\n--------\n\n"
-  return output;
+  // stylize the title
+  lines[0] = "## " + lines[0].split(' ').map(capitalize).join(' ')  + "\n\n"
+  return lines.join('  \n') + "\n\n---\n---\n\n"
 }
 
 let downloadPoem = function(titles) {
@@ -72,14 +76,22 @@ let downloadPoem = function(titles) {
     return;
   }
   request('http://poetrydb.org/title/' + encodeURIComponent(title), function(err, response, body) {
-    body = JSON.parse(body)[0];
-    console.log(parsePoem(body.lines));
+    if (err) {
+      console.error(err);
+    }
+    try {
+      body = JSON.parse(body);
+    } catch (err) {
+      console.error(err);
+    }
+    if ( body[0] && body[0].lines ) {
+      console.log(parsePoem(body[0].lines));
+    }
     downloadPoem(titles);
   });
 
 };
 
-console.log("getting titles...");
 request('http://poetrydb.org/title', function(err, response, body){
   if ( err ) {
     console.log(err);
