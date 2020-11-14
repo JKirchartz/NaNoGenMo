114590e46a24d2b1146f6d716d5b1a2eb2a3dc54
JKirchartz <jkirchartz@gmail.com>
2020-11-13 20:24:32 -0500

    start getting generative side together - analytics worked out well

---
+++ 2020/cosmic/textGen.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyleft (ↄ) 2020 jkirchartz <me@jkirchartz.com>
+#
+# Distributed under terms of the NPL (Necessary Public License) license.
+
+"""
+
+"""
+
