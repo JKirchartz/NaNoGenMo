8775094a717fa6d65c11c921aa6abdd9009422f3
JKirchartz <jkirchartz@gmail.com>
2020-11-30 22:56:31 -0500

    different translate package also errs out

--- 2020/Twainer/twainer.py
+++ 2020/Twainer/twainer.py
@@ -12,6 +12,7 @@
 
 import sys
 import re
+import textwrap
 from google_trans_new import google_translator
 
 translator = google_translator()
@@ -27,16 +28,23 @@ with open(sys.argv[1], 'r') as r:
         w.write("{}: in English, then in French, and then Clawed Back into a Civilized Language Once More by Patient, Unrenumerated Toil, done by a Robot".format(title))
         w.write(story + '\n\n\n\n')
 
-        french = translator.translate(story, 'fr', 'en')
+
+        lines = textwrap.wrap(story, 5000, break_long_words=False)
+        french=""
+        for line in lines:
+            french += translator.translate(line, 'fr', 'en')
 
         w.write(french + '\n\n\n\n')
 
-        french_words = list(set(rex.sub(' ', french).split())) # is this too much?
+        french_words = list(set(french.split())) # is this too much?
 
-        dic = translator.translate(". ".join(french_words), 'en', 'fr')
+        lines = textwrap.wrap(". ".join(french_words), 5000,
+                break_long_words=False)
+        for line in lines:
+            dic = translator.translate(line, 'en', 'fr')
 
         for word,key in dic.split(". "):
-            french=french.replace(french_words[key], word)
+            french += french.replace(french_words[key], word)
 
         w.write(french + '\n\n\n\n')
 
